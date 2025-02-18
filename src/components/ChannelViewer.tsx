c33a90d0dc1cfe352d9b09b626a8ca077c43fd67
yayacat <vcde0000@gmail.com>
2025-02-18 18:13:01 +0800

    fix: usergrade to UserGrade (#32)

--- src/components/ChannelViewer.tsx
+++ src/components/ChannelViewer.tsx
@@ -508,8 +508,8 @@ const UserTab: React.FC<UserTabProps> = React.memo(
                 className={`min-w-3.5 min-h-3.5 rounded-sm flex items-center justify-center ml-1`}
               >
                 <img
-                  src={`/usergrade_${userLevel}.png`}
-                  alt={`/usergrade_${userLevel}`}
+                  src={`/UserGrade_${userLevel}.png`}
+                  alt={`/UserGrade_${userLevel}`}
                   className="select-none"
                 />
               </div>
