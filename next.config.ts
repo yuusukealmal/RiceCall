a91bbe4979bdc6effbf6cd5ce65645862b65156d
夜夜 <vcde0000@gmail.com>
2025-02-28 00:31:31 +0800

    fix: electron-builder

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     domains: ['localhost'],
     // 如果你之後有其他網域也要加在這裡
     // 例如: 'your-production-domain.com'
+    unoptimized: true
   },
   basePath: '',
   assetPrefix: '',
