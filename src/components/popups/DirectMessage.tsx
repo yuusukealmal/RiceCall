9836a6adf41f21d7478390cf51a1eb144f24b1fe
Miyashooooo <cstrike.asia@gmail.com>
2025-04-01 11:46:14 +0800

    feat: DirectMessage (Nurge Not Work)

--- src/components/popups/DirectMessage.tsx
+++ src/components/popups/DirectMessage.tsx
@@ -19,6 +19,13 @@ import refreshService from '@/services/refresh.service';
 // Utils
 import { createDefault } from '@/utils/createDefault';
 
+// CSS
+import popup from '@/styles/common/popup.module.css';
+import directMessage from '@/styles/popups/directMessage.module.css';
+import vipLevel from '@/styles/common/vip.module.css';
+import userLevel from '@/styles/common/grade.module.css';
+import { dir } from 'console';
+
 interface DirectMessageModalProps {
   friendId: string;
   userId: string;
@@ -84,7 +91,161 @@ const DirectMessageModal: React.FC<DirectMessageModalProps> = React.memo(
       refresh();
     }, [userId, friendId]);
 
-    return null;
+    const handleNudgeEvent = () => {};
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const btn = document.createElement('button');
+      btn.onclick = () => shakeWindow('chatWindow');
+      document.body.appendChild(btn);
+    });
+    function shakeWindow(targetId: unknown, duration = 500) {
+      const windowEl = document.getElementById(targetId);
+      if (!windowEl) return;
+
+      const start = Date.now();
+      const shake = () => {
+        const elapsed = Date.now() - start;
+        if (elapsed > duration) {
+          windowEl.style.transform = 'translate(0, 0)';
+          return;
+        }
+
+        const x = (Math.random() - 0.5) * 30; // 範圍 -5~5px
+        const y = (Math.random() - 0.5) * 30;
+        windowEl.style.transform = `translate(${x}px, ${y}px)`;
+
+        requestAnimationFrame(shake);
+      };
+
+      shake();
+    }
+
+    return (
+      <>
+        <div className={directMessage['directBox']}>
+          <div className={directMessage['headerWrapper']}>
+            <div className={directMessage['headerBox']}>
+              <div className={directMessage['header']}>
+                <div className={directMessage['targetUserInfo']}>
+                  <span className={directMessage['targetUserName']}>123</span>
+                  <span className={directMessage['targetUserDescription']}>
+                    test message 123123121312323132
+                  </span>
+                </div>
+              </div>
+              <div className={directMessage['actionButtons']}>
+                <div
+                  className={`${directMessage['actionButton']} ${directMessage['minimizeButton']}`}
+                ></div>
+                <div
+                  className={`${directMessage['actionButton']} ${directMessage['maxsizeButton']}`}
+                ></div>
+                <div
+                  className={`${directMessage['actionButton']} ${directMessage['closeButton']}`}
+                ></div>
+              </div>
+            </div>
+            <div className={directMessage['directOptionButtons']}>
+              <div
+                className={directMessage['directOptionFileShareButton']}
+              ></div>
+              <div
+                className={directMessage['directOptionBlockUserButton']}
+              ></div>
+              <div
+                className={directMessage['directOptionInviteTempGroupButton']}
+              ></div>
+              <div className={directMessage['directOptionReportButton']}></div>
+            </div>
+          </div>
+          <div className={directMessage['bodyWrapper']}>
+            <div className={directMessage['bodyBox']}>
+              <div className={directMessage['disrectLeftItems']}>
+                <div className={directMessage['disrectLeftTargetBox']}>
+                  <div
+                    className={directMessage['disrectLeftTargetAvatarBorder']}
+                  >
+                    <div
+                      className={directMessage['disrectLeftTargetAvatarImage']}
+                    ></div>
+                  </div>
+                  <div
+                    className={`${vipLevel['vip-big-1']} ${directMessage['disrectTargetVIP']}`}
+                  ></div>
+                  <div
+                    className={`${userLevel['grade']} ${userLevel['lv-2']} ${directMessage['disrectTargetLevel']}`}
+                  ></div>
+                </div>
+                <div className={directMessage['disrectLeftSelfAvatarBorder']}>
+                  <div
+                    className={directMessage['disrectLeftSelfAvatarImage']}
+                  ></div>
+                </div>
+              </div>
+              <div className={directMessage['disrectRightItems']}>
+                <div className={directMessage['serverInBox']}>
+                  <div className={directMessage['serverInIcon']}></div>
+                  <div className={directMessage['serverInName']}>測試</div>
+                </div>
+                <div className={directMessage['serverSplit']}></div>
+                <div className={directMessage['chatTextArea']}>
+                  {/** 訊息內容 **/}
+                  <div className={directMessage['chatMessageBox']}>
+                    <div className={directMessage['chatMessageUsernameBox']}>
+                      <div className={directMessage['chatMessageUsername']}>
+                        你好名字
+                      </div>
+                      <div className={directMessage['chatMessageTime']}>
+                        17:15:17
+                      </div>
+                    </div>
+                    <div className={directMessage['chatMessageText']}>123</div>
+                  </div>
+                </div>
+                <div className={directMessage['chatInputBox']}>
+                  <div className={directMessage['chatInputTopBar']}>
+                    <div className={directMessage['chatInputTopBarLeft']}>
+                      <div
+                        className={`${directMessage['button']} ${directMessage['chatInputTopBarFont']}`}
+                      ></div>
+                      <div
+                        className={`${directMessage['button']} ${directMessage['chatInputTopBarEmoji']}`}
+                      ></div>
+                      <div
+                        className={`${directMessage['button']} ${directMessage['chatInputTopBarScreenShot']}`}
+                      ></div>
+                      <div
+                        className={`${directMessage['button']} ${directMessage['chatInputTopBarNudge']}`}
+                      ></div>
+                    </div>
+                    <div
+                      className={directMessage['chatInputTopBarHistoryMessage']}
+                    >
+                      訊息紀錄
+                    </div>
+                  </div>
+                  <div className={directMessage['chatInputTopBarInputArea']}>
+                    <textarea
+                      className={directMessage['chatInputTopBarTextarea']}
+                    ></textarea>
+                    <div
+                      className={
+                        directMessage['chatInputTopBarInputEnterButton']
+                      }
+                    ></div>
+                  </div>
+                </div>
+                <div className={directMessage['adSlogenArea']}>
+                  <div className={directMessage['adSlogenAreaText']}>
+                    這個遊戲我已經通霄玩了1分鐘，我先出殯了，推薦給你！
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
     // <Modal title={friendName} onClose={onClose} width="600px" height="600px">
     //   <div className="flex h-full">
     //     {/* Side Menu */}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2240,7 +2240,7 @@ export const PopupSize = {
   [PopupType.ADD_FRIEND]: { height: 220, width: 400 },
   [PopupType.ADD_FRIEND_GROUP]: { height: 220, width: 400 },
   [PopupType.EDIT_FRIEND_GROUP]: { height: 220, width: 400 },
-  [PopupType.DIRECT_MESSAGE]: { height: 200, width: 300 },
+  [PopupType.DIRECT_MESSAGE]: { height: 550, width: 650 },
   [PopupType.DIALOG_ALERT]: { height: 220, width: 400 },
   [PopupType.DIALOG_ALERT2]: { height: 220, width: 400 },
   [PopupType.DIALOG_SUCCESS]: { height: 220, width: 400 },
