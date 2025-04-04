b991cfb144dd0e38e2cbd1be0d373335caf41ad3
Whydog <142924636+JoshHuang9508@users.noreply.github.com>
2025-04-04 23:07:49 +0800

    fix: 修正 XP 重計算邏輯

--- xpReCalculate.js
+++ xpReCalculate.js
@@ -5,9 +5,11 @@ const Set = require('./utils/set.js');
 
 function totalXP(level, baseXP = 5, growthRate = 1.02) {
   if (level < 1) return 0;
-  return Math.round(
-    (baseXP * (1 - Math.pow(growthRate, level))) / (1 - growthRate),
-  );
+  let totalXP = 0;
+  for (let i = 1; i <= level; i++) {
+    totalXP += Math.ceil(baseXP * Math.pow(growthRate, i));
+  }
+  return totalXP;
 }
 
 function getRequiredXP(level) {
